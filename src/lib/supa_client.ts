03551c182ec58eea0fae71492009d7ac6851368b
Scott Tolinski <scott@leveluptuts.com>
2022-12-10 21:29:54 -0700

    adds console log

--- src/lib/supa_client.ts
+++ src/lib/supa_client.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
 import { env } from '$env/dynamic/public'
+console.log('env', env)
 console.log('PUBLIC_SUPABASE_ANON_KEY', env.PUBLIC_SUPABASE_ANON_KEY)
 
 export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
