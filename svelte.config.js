3a6e500aa920bc32595474ff388ef9f8c5f051fc
Scott Tolinski <scott@leveluptuts.com>
2022-12-10 20:50:27 -0700

    changes to adapter auto for Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-auto'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
-		prerender: { entries: [] },
+		adapter: adapter(),
 
 		alias: {
 			$state: './src/lib/state'
