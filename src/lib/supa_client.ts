40d3e0dec4aeefcb5459fc4cd54c283c268c98a5
Scott Tolinski <scott@leveluptuts.com>
2022-12-10 20:43:52 -0700

    makes env vars static

--- src/lib/supa_client.ts
+++ src/lib/supa_client.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/auth-helpers-sveltekit';
-import { env } from '$env/dynamic/public';
+import { createClient } from '@supabase/auth-helpers-sveltekit'
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
 
-export const supabase = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
