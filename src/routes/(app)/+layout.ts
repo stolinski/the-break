65ad63bff5b330f613e3a062758ffa6890397f7e
Scott Tolinski <scott.tolinski@gmail.com>
2024-07-08 22:01:41 -0600

    removes pointless refresh from every request

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -5,7 +5,6 @@ export const ssr = false
 
 export const load = async function ({ depends }) {
 	depends('app:user')
-	if (pb.authStore.isValid) await pb.collection('users').authRefresh()
 	return {
 		user: pb.authStore.isValid ? (pb.authStore.model as UsersResponse) : undefined
 	}
